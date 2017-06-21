9c946d9b3a8f9caf9a9b461d6227aec43e287885
tibyte <ttl74283@gmail.com>
2017-06-21 20:49:55 +0900

    Test savebtn3

--- js/envcheck.js
+++ js/envcheck.js
@@ -29,12 +29,11 @@ function isEdge()
 function isMobile()
 {
     var filter = "win16|win32|win64|mac|macintel";
-        if(navigator.platform) {
-            if(filter.indexOf(navigator.platform.toLowerCase()) < 0) {
-                return true;
-            } else {
-                return false;
-            }
+    if(navigator.platform) {
+        if(filter.indexOf(navigator.platform.toLowerCase()) < 0) {
+            return true;
+        } else {
+            return false;
         }
     }
 }
--- js/index.js
+++ js/index.js
@@ -413,7 +413,7 @@ function onClickBtnSave()
     if (("download" in $("#btnSave").get(0)) && !isEdge() && !isMobile()) {
         var dt = canvas.toDataURL("image/png");
         //this.href = dt;
-        alert(this.href);
+        alert("[" + dt + "]");
     }
     else {
         var p = openPopup("down.html", 505, 705);
